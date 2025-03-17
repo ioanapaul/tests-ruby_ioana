7d278b446c39fc969b2fe08743ba141c264fb67a
Paul <84133282+ioanapaul@users.noreply.github.com>
2025-03-17 18:43:25 +0100

    Add files via upload

---
+++ lib/01_temperature.rb
@@ -0,0 +1,11 @@
+# temperature.rb
+
+# Conversion de Fahrenheit vers Celsius
+def ftoc(f)
+  (f - 32) * 5.0 / 9.0
+end
+
+# Conversion de Celsius vers Fahrenheit
+def ctof(c)
+  c * 9.0 / 5.0 + 32
+end
