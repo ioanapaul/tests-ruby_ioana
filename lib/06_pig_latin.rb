c59b4c5aafeb4f31dae2db4df373d1b24cb7fcfc
Paul <84133282+ioanapaul@users.noreply.github.com>
2025-03-18 09:06:27 +0100

    Update 06_pig_latin.rb

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,30 +1,22 @@
-def translate(phrase)
-    # Diviser la phrase en mots
-    words = phrase.split(" ")
-  
-    # Traduire chaque mot
-    translated_words = words.map do |word|
-      # Si le mot commence par une voyelle
-      if word[0].match(/[aeiouAEIOU]/)
-        word + "ay"
-      # Si le mot commence par 'sch'
-      elsif word.start_with?("sch") || word.start_with?("Sch")
-        word[3..-1] + "schay"
-      # Si le mot commence par 'qu'
-      elsif word.start_with?("qu") || word.start_with?("Qu")
-        word[2..-1] + "quay"
-      # Sinon, on déplace les consonnes au début du mot
+def translate(word)
+  words = word.split(" ")
+  vowels = ["a", "e", "i", "o", "u"]
+
+  translated_words = words.map do |w|
+    if vowels.include?(w[0]) # commence par une voyelle
+      w + "ay"
+    else
+      qu_index = w.index("qu")
+      if qu_index == 0
+        w[2..] + "quay" # "quiet" → "ietquay"
+      elsif qu_index
+        w[(qu_index + 2)..] + w[0...qu_index] + "quay" # "square" → "uaresquay"
       else
-        # Trouver la première voyelle
-        first_vowel_index = word.index(/[aeiouAEIOU]/)
-        if first_vowel_index
-          word[first_vowel_index..-1] + word[0...first_vowel_index] + "ay"
-        else
-          word + "ay"
-        end
+        consonants = w[/\A[^aeiou]+/]
+        w[consonants.length..] + consonants + "ay"
       end
     end
-  
-    # Rejoindre les mots traduits pour faire une phrase
-    translated_words.join(" ")
   end
+
+  translated_words.join(" ")
+end
